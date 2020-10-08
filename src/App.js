327a4159f9edd9728bce7c272eac02be2a923817
Tamas Farago <tamas@Tamass-MacBook-Pro.local>
2020-10-08 21:38:50 +0200

    created project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      hi
+      <button>hello</button>
+    </div>
+  );
+}
+
+export default App;
