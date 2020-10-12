a65313b34c8bf0b31b87e7e1e4a3d4c374277c9e
Tamas Farago <tamas@Tamass-MacBook-Pro.local>
2020-10-12 16:33:16 +0200

    finished modal styling

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -5,6 +5,9 @@ import project1 from "../images/project1.png"
 import project2 from "../images/project2.png"
 import project3 from "../images/project3.png"
 import project4 from "../images/project-4-big.png"
+import modal1 from "../images/project-1-modal.png"
+import { AiOutlineClose } from "react-icons/ai"
+import { FaEye, FaCode } from "react-icons/fa"
 
 export default class Portfolio extends Component {
     render() {
@@ -51,6 +54,44 @@ export default class Portfolio extends Component {
                                         <button className="project-btn btn4">View Project</button>
                                     </div>
                                 </div>
+                        </div>
+                        <div className="project-modal modal1">
+                            <div className="modal-grid">
+                                <div className="modal-picture">
+                                    <img src={modal1}></img>
+                                </div>
+                                <div className="modal-info">
+                                    <div className="project-header">
+                                        <div className="modal-title">
+                                            <h5>project</h5>
+                                            <h3>Ecommerce website</h3>
+                                        </div>
+                                        <AiOutlineClose className="modal-close" />
+                                    </div>
+                                    <ul className="project-technology">
+                                        <li>html</li>
+                                        <li>css</li>
+                                        <li>React</li>
+                                        <li>material-ui</li>
+                                        <li>responsive</li>
+                                        <li>javascript</li>
+                                        <li>responsive</li>
+                                        <li>UX/UI</li>
+                                    </ul>
+                                    <div className="modal-about">
+                                        <h5>about</h5>
+                                        <div className="about-text">
+                                            <p>I used ReactJs to create this project. The purpose was to showcase my skills in javascript. The project has a very simple but modern UI which was created with only html and css. Has all the functionalities an eCommerce website has. Including shopping-cart, search filter, comment section etc.</p>
+                                        </div>
+                                    </div>
+                                    <div className="modal-buttons">
+                                        <button className="modal-btn-1"><FaEye className="eye"/>  demo</button>
+                                        <button className="modal-btn-2"><FaCode className="code" /> code</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                            
                         </div>
                        
                     </div>
