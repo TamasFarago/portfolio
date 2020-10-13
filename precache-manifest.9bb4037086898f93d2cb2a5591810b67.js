1afe47f642541d270e35b025e64d40d08e705857
Tamas Farago <67281301+TamasFarago@users.noreply.github.com>
2020-10-13 14:20:26 +0200

    Updates

---
+++ precache-manifest.9bb4037086898f93d2cb2a5591810b67.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d1113edc6d27f690694b1ab1c05b68",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2cd1bb2f6e86c2f1b568",
+    "url": "/portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "7eb040bc18f38fc8cb01",
+    "url": "/portfolio/static/css/main.811c56e1.chunk.css"
+  },
+  {
+    "revision": "2cd1bb2f6e86c2f1b568",
+    "url": "/portfolio/static/js/2.5a9f8282.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.5a9f8282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eb040bc18f38fc8cb01",
+    "url": "/portfolio/static/js/main.af764f99.chunk.js"
+  },
+  {
+    "revision": "eb0aafffcb038f123bf0",
+    "url": "/portfolio/static/js/runtime-main.255623fe.js"
+  },
+  {
+    "revision": "9aaa7348e3a2f9720513cc77a837fe05",
+    "url": "/portfolio/static/media/logo.9aaa7348.png"
+  },
+  {
+    "revision": "8aff42ed3d67e7c888eebf0b181fdaeb",
+    "url": "/portfolio/static/media/project-1-modal.8aff42ed.png"
+  },
+  {
+    "revision": "7c7f18aac368ebbeab28ff076ccba143",
+    "url": "/portfolio/static/media/project-2-modal.7c7f18aa.png"
+  },
+  {
+    "revision": "39fe93f7d8cbedb5d751750833580aff",
+    "url": "/portfolio/static/media/project-3-modal.39fe93f7.png"
+  },
+  {
+    "revision": "f598baddb3969a2a80df3596b79768f4",
+    "url": "/portfolio/static/media/project-4-big.f598badd.png"
+  },
+  {
+    "revision": "9f898e967d586b9b77828eb53eadc1b5",
+    "url": "/portfolio/static/media/project-4-modal.9f898e96.jpg"
+  },
+  {
+    "revision": "d615179c6280af3c7b5694cb24bf9038",
+    "url": "/portfolio/static/media/project1.d615179c.png"
+  },
+  {
+    "revision": "5efd06b45059fd5a424cfff4781cc57a",
+    "url": "/portfolio/static/media/project2.5efd06b4.png"
+  },
+  {
+    "revision": "fd2f5cb69f052eca7cc4069b59ea0f41",
+    "url": "/portfolio/static/media/project3.fd2f5cb6.png"
+  }
+]);
